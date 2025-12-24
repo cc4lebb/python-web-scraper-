a3cfd40e7f34604931e3457547e7efc1ae6bc9b2
Caleb Brock <calebbrock19@gmail.com>
2025-12-24 15:01:52 +0800

    added a for loop to look through each website. need to find the url ammount in the html/css using beautiful soup

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urls import url_list
 
-response = requests.get(url_list[1])
-soup = bs(response.text, 'html.parser')
-print(soup)
+for url in url_list:
+    response = requests.get(url)
+    soup = bs(response.text, 'html.parser')
+    urlAmount = soup.find_all("") #need to find element of html/ css here to print the price from each website
+    print(urlAmount)
