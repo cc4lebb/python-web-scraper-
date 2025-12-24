3a2956ad28154d53d3fab3c802c6a38acae2cdbb
Caleb Brock <calebbrock19@gmail.com>
2025-12-24 14:06:57 +0800

    website to scrape

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# website to scrape : https://www.jbhifi.com.au/products/apple-airpods-pro-3
+
+import requests
+
+url = "https://www.jbhifi.com.au/products/apple-airpods-pro-3"
+
+response = requests.get(url)
+print(response)
