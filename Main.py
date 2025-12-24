13c295ab6fdec64431555c0d2573b1d850acfad6
Caleb Brock <calebbrock19@gmail.com>
2025-12-25 00:26:16 +0800

    formatting changes for displaying the price by adding a domain name before the price is shown

--- Main.py
+++ Main.py
@@ -2,8 +2,22 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urls import url_list
 
+keyWord = "== $0"
+
+
 for url in url_list:
     response = requests.get(url)
     soup = bs(response.text, 'html.parser')
-    urlAmount = soup.find_all("") #need to find element of html/ css here to print the price from each website
-    print(urlAmount)
+
+    startOfDomain = url.find('.')
+    endOfDomain = url.find(".com")
+    domainName = url[startOfDomain + 1:endOfDomain]
+    print(f"{domainName}: ")
+
+
+
+
+#need to find element of html/ css here to print the price from each website
+'''
+span, class. use the in operator to extract the text within the html line to find the word price 
+'''
