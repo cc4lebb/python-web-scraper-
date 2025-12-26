4eeef943f1789927f2be550809214952bf580eaa
Caleb Brock <calebbrock19@gmail.com>
2025-12-26 22:27:24 +0800

    testing with the amazon html/ css line

--- Main.py
+++ Main.py
@@ -2,22 +2,16 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urls import url_list
 
-keyWord = "== $0"
-
-
 for url in url_list:
     response = requests.get(url)
     soup = bs(response.text, 'html.parser')
 
+    price = soup.find("span", "a-price-whole") #price element
+
     startOfDomain = url.find('.')
     endOfDomain = url.find(".com")
     domainName = url[startOfDomain + 1:endOfDomain]
-    print(f"{domainName}: ")
+    print(f"{domainName}: {price}")
 
 
 
-
-#need to find element of html/ css here to print the price from each website
-'''
-span, class. use the in operator to extract the text within the html line to find the word price 
-'''
